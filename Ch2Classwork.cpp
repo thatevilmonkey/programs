701e96cdf71497b62e8cd6d28e0f6e0460b7e712
Evil Monkey <evilmonkey@protonmail.com>
2015-12-13 23:49:49 -0800

    Add Ch2Classwork.cpp

---
+++ Ch2Classwork.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    double amount=0,
+    cout<<"Please enter the amount "<<endl;
+    cin>>
+    cout<<"\"C++\" is \nfun. \n"
+        <<"Use \"\\\" before n to go to new line \n";
+
+return 0;
+}
